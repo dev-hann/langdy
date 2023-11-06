233d66c3ad0caaa7c9e328c9169f26b461e05630
yoehwan <yoehwan@gmail.com>
2023-11-06 11:42:20 +0800

    :fire: Remove: ticket code

--- lib/provider/home_provider.dart
+++ lib/provider/home_provider.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:langdy/enum/page_type.dart';
-import 'package:langdy/widget/ticket_float_widget.dart';
 
 class HomeProvider extends ChangeNotifier {
   int _pageIndex = 0;
@@ -18,43 +17,6 @@ class HomeProvider extends ChangeNotifier {
   // FIXME: Need Refactoring..
   void moveToPage(PageType value) {
     _pageType = value;
-    if (value == PageType.community) {
-      _ticketVisibility = TicketVisibility.hide;
-    } else {
-      _ticketVisibility = TicketVisibility.show;
-    }
-    notifyListeners();
-  }
-
-  TicketVisibility _ticketVisibility = TicketVisibility.hide;
-
-  TicketVisibility get ticketVisibility => _ticketVisibility;
-
-  void hideTicket() {
-    _ticketVisibility = TicketVisibility.hide;
-    notifyListeners();
-  }
-
-  void showTicket() {
-    // if (pageType == PageType.community) {
-    //   return;
-    // }
-    // _ticketVisibility = TicketVisibility.show;
-    // notifyListeners();
-  }
-
-  TicketMode _ticketMode = TicketMode.normal;
-  TicketMode get ticketMode => _ticketMode;
-
-  bool _overlayTicket = false;
-  bool get isOverlayTicket => _overlayTicket;
-  void showTicketOverlay() {
-    _overlayTicket = true;
-    notifyListeners();
-  }
-
-  void hideOverlayTicket() {
-    _overlayTicket = false;
     notifyListeners();
   }
 }
--- lib/view/main_view.dart
+++ lib/view/main_view.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:langdy/enum/page_type.dart';
 import 'package:langdy/provider/home_provider.dart';
 import 'package:langdy/view/booking_view/booking_view.dart';
 import 'package:langdy/view/community_view/community_view.dart';
 import 'package:langdy/view/home_view/home_view.dart';
 import 'package:langdy/view/user_view/user_view.dart';
-import 'package:langdy/widget/ticket_float_widget.dart';
-import 'package:langdy/widget/ticket_overlay.dart';
 import 'package:provider/provider.dart';
 
 class MainView extends StatelessWidget {
@@ -42,110 +39,28 @@ class MainView extends StatelessWidget {
     );
   }
 
-  Widget ticketOverlay({
-    required bool isOverlay,
-    required VoidCallback onTapDismiss,
-  }) {
-    if (!isOverlay) {
-      return const SizedBox();
-    }
-
-    return TicketOverlay(
-      onTapDismiss: onTapDismiss,
-    );
-  }
-
-  Widget body({
-    required PageType pageType,
-  }) {
-    return Builder(
-      builder: (context) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          initialRoute: HomeView.name,
-          theme: Theme.of(context),
-          // routes: {
-          // TownDetailView.name: (_) => const TownDetailView(),
-          // },
-          home: Material(
-            child: Builder(
-              builder: (context) {
-                switch (pageType) {
-                  case PageType.home:
-                    return const HomeView();
-                  case PageType.booking:
-                    return const BookingView();
-                  case PageType.community:
-                    return const CommunityView();
-                  case PageType.user:
-                    return const UserView();
-                }
-              },
-            ),
-          ),
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<HomeProvider>(context);
     final pageType = provider.pageType;
     return SafeArea(
       top: true,
-      child: NotificationListener<UserScrollNotification>(
-        onNotification: (notification) {
-          final dicrection = notification.direction;
-          switch (dicrection) {
-            case ScrollDirection.idle:
-              return true;
-            case ScrollDirection.forward:
-              provider.showTicket();
-              return true;
-            case ScrollDirection.reverse:
-              provider.hideTicket();
-              return true;
-          }
-        },
-        child: Scaffold(
-          body: Stack(
-            children: [
-              body(pageType: pageType),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: TicketFloatWidget(
-                  onTap: () {
-                    provider.showTicketOverlay();
-                  },
-                  ticketVisibility: provider.ticketVisibility,
-                  ticketMode: provider.ticketMode,
-                ),
-              ),
-              ticketOverlay(
-                isOverlay: provider.isOverlayTicket,
-                onTapDismiss: () {
-                  provider.hideOverlayTicket();
-                },
-              ),
-            ],
-          ),
-          bottomNavigationBar: bottom(
-            currentIndex: provider.pageType.index,
-            onTap: (index) {
-              provider.moveToPage(PageType.values[index]);
-              // provider.updatePageIndex(index);
-
-              // final pageList = [
-              //   HomeView.name,
-              //   BookingView.name,
-              //   CommunityView.name,
-              //   UserView.name,
-              // ];
-              // print(pageList[index]);
-              // Navigator.pushNamed(context, pageList[index]);
-              // final pageType = PageType.values[index];
-              // provider.moveToPage(pageType);
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: Theme.of(context),
+        home: Material(
+          child: Builder(
+            builder: (context) {
+              switch (pageType) {
+                case PageType.home:
+                  return const HomeView();
+                case PageType.booking:
+                  return const BookingView();
+                case PageType.community:
+                  return const CommunityView();
+                case PageType.user:
+                  return const UserView();
+              }
             },
           ),
         ),
